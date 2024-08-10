3397b8a580fa6626a73887b113059799fb93db12
CSeungJoo <seungjoo13579@gmail.com>
2024-08-10 16:21:43 +0900

    종류 2 통과

--- src/_3.java
+++ src/_3.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class _3 {
-    public static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,29 +12,39 @@ public class _3 {
             int B = sc.nextInt();
 
             System.out.println(solution(A, B, 1));
-            result = Integer.MAX_VALUE;
         }
     }
     public static int solution(int a, int b, int cnt) {
-        int[][] dxdy = {{1, 1}, {2, 0}, {0, 2}, {1, 0}, {0, 1}};
-
-        if (--a < 0 || --b < 0)
+        if (3 < Math.max(a, b) / Math.min(a, b))
             return -1;
-
-        if (a + b <= 2) {
-            if (cnt < result)
-                result = cnt;
-            return cnt;
-        }else {
-            for (int i = 0; i < dxdy.length; i++) {
-                int value = solution(a - dxdy[i][0], b - dxdy[i][1], cnt + 1);
-
-                if (value != -1)
-                    solution(a - dxdy[i][0], b - dxdy[i][1], cnt + 1);
-            }
-        }
-        if (result == Integer.MAX_VALUE)
-            return -1;
-        return result;
+        else
+            return (a + b + 3) / 4;
     }
 }
+// 1 1
+// 1 2
+// 1 3
+// 1 4 x
+// 2 1
+// 2 2
+// 2 3
+// 2 4
+// 2 5
+// 2 6
+// 2 7 x
+// 3 1
+// 3 2
+// 3 3
+// 4 1 x
+// 4 2
+// 4 3
+// 4 4
+// 5 1 x
+// 5 2
+// 5 3
+// 5 4
+// 5 5
+// 6 1 x
+// 6 2
+// 7 2 x
+// 7 3
