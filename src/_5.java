919499a1d5f85a93cf0a31633c8d3ba19d3dc370
CSeungJoo <seungjoo13579@gmail.com>
2024-08-10 15:18:01 +0900

    종류 2, 3 통과

--- src/_5.java
+++ src/_5.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class _5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] client = new int[N];
+        long[] client = new long[N];
         for (int i = 0; i < N; i++) {
             client[i] = sc.nextInt();
         }
@@ -13,15 +13,15 @@ public class Main {
         System.out.println(solution(client));
     }
 
-    public static int solution(int[] client) {
+    public static long solution(long[] client) {
         int N = client.length;
 
         switch (N) {
-            case 1: return client[0];
-            case 2: return Math.max(client[1], client[0]);
+            case 1: return (int) client[0];
+            case 2: return (int) Math.max(client[1], client[0]);
         }
 
-        int[] dp = new int[N];
+        long[] dp = new long[N];
 
         dp[0] = client[0];
         dp[1] = Math.max(client[1], client[0]);
