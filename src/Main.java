5ffe2e40373e03f5a93b546eb7d855d0a8b43b21
CSeungJoo <seungjoo13579@gmail.com>
2024-08-10 14:56:33 +0900

    종류 1 통과

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-
         int[] client = new int[N];
         for (int i = 0; i < N; i++) {
             client[i] = sc.nextInt();
         }
         sc.close();
 
-
-        System.out.println(solution(0, 0, N, client));
+        System.out.println(solution(client));
     }
-    public static int solution(int index, int num, int maxIndex, int[] client) {
-        if (index == maxIndex - 1)
-            return num;
 
-        int one = solution(index + 1, num + client[index + 1], maxIndex, client);
-        int two = Integer.MAX_VALUE;
-        if (index != maxIndex - 2)
-            two= solution(index + 2, num + (client[index + 1] > client[index + 2] ? client[index + 1] : client[index + 2]), maxIndex, client);
+    public static int solution(int[] client) {
+        int N = client.length;
 
-        return one < two ? one : two;
+        switch (N) {
+            case 1: return client[0];
+            case 2: return Math.max(client[1], client[0]);
+        }
+
+        int[] dp = new int[N];
+
+        dp[0] = client[0];
+        dp[1] = Math.max(client[1], client[0]);
+
+        for (int i = 2; i < N; i++) {
+            dp[i] = Math.min(dp[i - 1] + client[i], dp[i - 2] + Math.max(client[i], client[i - 1]) );
+        }
+        return dp[N - 1];
     }
 }
